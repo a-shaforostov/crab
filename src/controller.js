3acefd5b8904d5507c575d7d7cfc3449c0fce46c
andrii.shaforostov <andpr99@gmail.com>
2018-05-13 23:04:43 +0300

    turn off cerebral debugger

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,15 @@
+// Uncomment lines to attach cerebral debugger
+// Commented to avoid console errors in development mode
+
 import { Controller } from "cerebral";
-import Devtools from "cerebral/devtools";
+// import Devtools from "cerebral/devtools";
 import app from "./app/index";
 
 const controller = Controller(app, {
-  devtools: navigator.userAgent.toLowerCase().includes("chrome")
-    ? Devtools({ host: "localhost:8686" })
-    : null
+  // devtools: navigator.userAgent.toLowerCase().includes("chrome")
+  //   ? Devtools({ host: "localhost:8686" })
+  //   : null
+  devtools: null,
 });
 
 export default controller;
